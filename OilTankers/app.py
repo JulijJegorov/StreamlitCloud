877767116ebd597aec88ece09d98740802345c54
Julij Jegorov <julij.jegorov@gmail.com>
2022-12-30 20:00:49 +0000

    Update app.py

--- OilTankers/app.py
+++ OilTankers/app.py
@@ -4,3 +4,4 @@ import streamlit as st
 st.set_page_config(page_icon="🐤", page_title="Twitter Sentiment Analyzer new")
 
 st.write('<base target="_blank">', unsafe_allow_html=True)
+st.text('Hi Streamlit Cloud')
