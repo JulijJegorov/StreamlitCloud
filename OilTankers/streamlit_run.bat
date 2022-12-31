61d193e0b1745866ad33c0f9ee438d255e75937d
Julij Jegorov <julij.jegorov@gmail.com>
2022-12-31 09:34:17 +0000

    update

--- OilTankers/streamlit_run.bat
+++ OilTankers/streamlit_run.bat
@@ -1,3 +1,2 @@
 SET PYTHONPATH=%PYTHONPATH%;C:\Users\julij\OneDrive\Documents\MyDocuments\Coding\python\trunk\QuantResearch\models\
-CALL C:\Users\julij\virtualenv\MLDS\Scripts\activate.bat
 streamlit run %~dp0\app.py
