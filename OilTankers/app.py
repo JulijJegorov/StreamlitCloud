ad51a4b8f26c3866fbc91b7802b497ccfe3153c8
Julij Jegorov <julij.jegorov@gmail.com>
2022-12-31 09:38:35 +0000

    Update app.py

--- OilTankers/app.py
+++ OilTankers/app.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import streamlit as st
 from yolonet import YoloNet
 
@@ -8,3 +9,9 @@ st.set_page_config(page_icon="🐤", page_title="Twitter Sentiment Analyzer new"
 
 st.write('<base target="_blank">', unsafe_allow_html=True)
 st.text('Hi Streamlit Cloud')
+st.text(__location__)
+
+model = YoloNet(lr=2.5e-5, weight_decay=1e-4)
+
+
+model.load_state_dict(torch.load(f'{__location__}/yolonet_.pt'))
