948dadddf3ddbd7da2a9f9fbee950d9ad0ea45ec
Julij Jegorov <julij.jegorov@gmail.com>
2022-12-31 09:35:25 +0000

    Update app.py

--- OilTankers/app.py
+++ OilTankers/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from yolonet import YoloNet
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-pip
+
 st.set_page_config(page_icon="🐤", page_title="Twitter Sentiment Analyzer new")
 
 st.write('<base target="_blank">', unsafe_allow_html=True)
