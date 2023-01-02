e95d13a1cd8158b03c61899814fbdd03f5086efe
Julij Jegorov <julij.jegorov@gmail.com>
2023-01-02 16:37:04 +0000

    Update app.py

--- OilTankers/app.py
+++ OilTankers/app.py
@@ -44,7 +44,7 @@ def plot_bboxes(test_dataset, remove_rectangles, slider_sides_diff):
 
         pixel_values, target = test_dataset[random_idx]
         pixel_values = pixel_values.unsqueeze(0)
-        image = annotate_image_predicted(yolo_model, pixel_values, image_path, 0.000000000005, remove_rectangles, slider_sides_diff)
+        image = annotate_image_predicted(yolo_model, pixel_values, image_path, 0.0, remove_rectangles, slider_sides_diff)
         images_pred.append(image)
 
     st.markdown('**Annotated Bounding Boxes**')
