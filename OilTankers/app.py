dee54c65fd32b39661dc8d6fbe9e83dabb9af7bd
Julij Jegorov <julij.jegorov@gmail.com>
2023-01-02 13:03:22 +0000

    add PIL

--- OilTankers/app.py
+++ OilTankers/app.py
@@ -1,11 +1,27 @@
 import os
 import torch
 import streamlit as st
+
 from yolonet import YoloNet
 from custom_dataset import CustomDataset
+from PIL import Image, ImageDraw, ImageFont
 from transformers import AutoFeatureExtractor
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def annotate_image(image_path: str, annotations: dict, categories: dict ):
+  image = Image.open(image_path)
+  image_draw = ImageDraw.Draw(image, 'RGBA')
+
+  for annotation in annotations:
+    box = annotation['bbox']
+    class_idx = annotation['category_id']
+    x, y, w, h = tuple(box)
+    image_draw.rectangle((x, y, x+w, y+h), outline='red', width=1)
+    image_draw.text((x, y), categories[class_idx], fill='white')
+  return image
+
+
 
 st.set_page_config(page_icon="🐤", page_title="Twitter Sentiment Analyzer new")
 
