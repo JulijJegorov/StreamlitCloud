6a7dbd3b0d39aa6e49af8b28e21cb26a2df911ff
Julij Jegorov <julij.jegorov@gmail.com>
2023-01-02 12:48:52 +0000

    rename annotation file

--- OilTankers/app.py
+++ OilTankers/app.py
@@ -2,6 +2,7 @@ import os
 import torch
 import streamlit as st
 from yolonet import YoloNet
+from custom_dataset import CustomDataset
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
@@ -11,7 +12,13 @@ st.write('<base target="_blank">', unsafe_allow_html=True)
 st.text('Hi Streamlit Cloud')
 st.text(__location__)
 
+
+dataset = CustomDataset(imgage_folder=(f'{__location__}/imgs'), annotation_file=f'{__location__}/labels_coco.json', feature_extractor=feature_extractor)
+
+
 model = YoloNet(lr=2.5e-5, weight_decay=1e-4, train_dataloader=None, valid_dataloader=None)
+
+
 
 
 model.load_state_dict(torch.load(f'{__location__}/yolonet_.pt'))
