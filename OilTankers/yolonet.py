12df63b33b380443223055ca5564f09529cc715b
Julij Jegorov <julij.jegorov@gmail.com>
2023-01-02 20:06:01 +0000

    simplify model

--- OilTankers/yolonet.py
+++ OilTankers/yolonet.py
@@ -16,44 +16,44 @@ class YoloNet(pl.LightningModule):
         self.model = AutoModelForObjectDetection.from_pretrained('hustvl/yolos-tiny',
                                                                  num_labels=1,
                                                                  ignore_mismatched_sizes=True)
-        self.lr = lr
-        self.weight_decay = weight_decay
-        self.train_dataloader = train_dataloader
-        self.valid_dataloader = valid_dataloader
-        self.save_hyperparameters()
+        # self.lr = lr
+        # self.weight_decay = weight_decay
+        # self.train_dataloader = train_dataloader
+        # self.valid_dataloader = valid_dataloader
+        # self.save_hyperparameters()
 
     def forward(self, pixel_values):
         outputs = self.model(pixel_values=pixel_values)
         return outputs
 
-    def common_step(self, batch, batch_idx):
-        pixel_values = batch['pixel_values']
-        labels = [{k: v.to(self.device) for k, v in t.items()} for t in batch["labels"]]
-        outputs = self.model(pixel_values=pixel_values, labels=labels)
-        loss = outputs.loss
-        loss_dict = outputs.loss_dict
-        return loss, loss_dict
-
-    def training_step(self, batch, batch_idx):
-        loss, loss_dict = self.common_step(batch, batch_idx)
-        self.log("train_loss", loss)
-        for k, v in loss_dict.items():
-            self.log('train_' + k, v.item())
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        loss, loss_dict = self.common_step(batch, batch_idx)
-        self.log("validation_loss", loss)
-        for k, v in loss_dict.items():
-            self.log("validation_" + k, v.item())
-        return loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
-        return optimizer
-
-    def train_dataloader(self):
-        return self.train_dataloader
-
-    def val_dataloader(self):
-        return self.valid_dataloader
+    # def common_step(self, batch, batch_idx):
+    #     pixel_values = batch['pixel_values']
+    #     labels = [{k: v.to(self.device) for k, v in t.items()} for t in batch["labels"]]
+    #     outputs = self.model(pixel_values=pixel_values, labels=labels)
+    #     loss = outputs.loss
+    #     loss_dict = outputs.loss_dict
+    #     return loss, loss_dict
+    #
+    # def training_step(self, batch, batch_idx):
+    #     loss, loss_dict = self.common_step(batch, batch_idx)
+    #     self.log("train_loss", loss)
+    #     for k, v in loss_dict.items():
+    #         self.log('train_' + k, v.item())
+    #     return loss
+    #
+    # def validation_step(self, batch, batch_idx):
+    #     loss, loss_dict = self.common_step(batch, batch_idx)
+    #     self.log("validation_loss", loss)
+    #     for k, v in loss_dict.items():
+    #         self.log("validation_" + k, v.item())
+    #     return loss
+    #
+    # def configure_optimizers(self):
+    #     optimizer = torch.optim.AdamW(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+    #     return optimizer
+    #
+    # def train_dataloader(self):
+    #     return self.train_dataloader
+    #
+    # def val_dataloader(self):
+    #     return self.valid_dataloader
