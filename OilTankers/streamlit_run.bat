9601e7889be4a0d77d68af6b6fcfaa9ec44feae3
Julij Jegorov <julij.jegorov@gmail.com>
2023-01-04 12:08:26 +0000

    remove app

--- OilTankers/streamlit_run.bat
+++
@@ -1,3 +0,0 @@
-SET PYTHONPATH=%PYTHONPATH%;C:\Users\julij\OneDrive\Documents\MyDocuments\Coding\python\trunk\QuantResearch\models\
-CALL C:\Users\julij\virtualenv\MLDS\Scripts\activate.bat
-streamlit run %~dp0\app.py
